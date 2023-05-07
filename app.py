ff9636e0b14ce40bbc08e84a31ee0e8a51b763ba
beczheng <beckie.zheng@stonybrook.edu>
2023-05-07 00:05:42 +0000

    updated the python file

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = FastAPI()
 df = pd.read_csv('./data/smallutilization2019.csv')
 
 @app.get("/")
-def home():
+async def home():
     return {"this is an API service for MN code details"}
 
 @app.route('/preview', methods=["GET"])
-def preview():
+async def preview():
     top10rows = df.head(10)
     result = top10rows.to_json(orient="records")
     return result
 
 @app.route('/icd/<value>', methods=['GET'])
-def icdcode(value):
+async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
@@ -26,7 +26,7 @@ def icdcode(value):
         return filtered.to_json(orient="records")
 
 @app.route('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+async def icdcode2(value, value2):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
