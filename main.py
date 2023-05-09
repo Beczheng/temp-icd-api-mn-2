7f213eb6876de2390a37fb7dd73a348cd94e17cb
beczheng <beckie.zheng@stonybrook.edu>
2023-05-09 01:50:20 +0000

    updated python file

--- main.py
+++ main.py
@@ -16,24 +16,24 @@ async def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.get('/icd/{value}')
+@app.get('/encounter_count/{value}')
 async def icdcode(value):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['encounter_count'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else:
         return filtered.to_json(orient="records")
 
-@app.get('/icd/{value}/payer/{value2}')
+@app.get('/icd/{value}/sex/{value2}')
 async def icdcode2(value, value2):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['payer'] == value2]
+    filtered = df[df['encounter_count'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else:
         return filtered2.to_json(orient="records")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=30000)
