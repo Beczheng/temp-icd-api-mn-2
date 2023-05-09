0d5bcd7b3278c455fd6ae0048d8c3974139d993b
beczheng <beckie.zheng@stonybrook.edu>
2023-05-09 01:21:03 +0000

    update the python file

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ df = pd.read_csv('./data/smallutilization2019.csv')
 async def home():
     return {"this is an API service for MN code details"}
 
-@app.route('/preview', methods=["GET"])
-def preview():
+@app.get('/preview')
+async def preview():
     top10rows = df.head(10)
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=["GET"])
-def icdcode(value):
+@app.get('/icd/{value}')
+async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
@@ -25,15 +25,12 @@ def icdcode(value):
     else:
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+@app.get('/icd/{value}/payer/{value2}')
+async def icdcode2(value, value2):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['sex'] == value2]
+    filtered2 = filtered[filtered['payer'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else:
         return filtered.to_json(orient="records")
-
-if __name__ == '__main__':
-    app.run(debug=True)
