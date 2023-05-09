de915228ea9a0face01cfe50ad19876111a667a7
beczheng <beckie.zheng@stonybrook.edu>
2023-05-09 02:00:08 +0000

    updated python file

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ async def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.get('/age_group/{value}')
+@app.get('/age_group_code/{value}')
 async def icdcode(value):
     print('value: ', value)
-    filtered = df[df['age_group'] == value]
+    filtered = df[df['age_group_code'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else:
         return filtered.to_json(orient="records")
 
-@app.get('/age_group/{value}/sex/{value2}')
+@app.get('/age_group_code/{value}/sex/{value2}')
 async def icdcode2(value, value2):
     print('value: ', value)
-    filtered = df[df['age_group'] == value]
+    filtered = df[df['age_group_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
